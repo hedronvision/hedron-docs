0ef69794816f4edee5987649f5c1d2a759fd2ae5
Sam Redmond <contact+github@sredmond.io>
2020-12-10 18:21:22 -0800

    Configure Sphinx, breathe, and exhale to generate C# documentation.
    This commit cleans up Sphinx's configuration options and sets up breathe and
    exhale to autogenerate documentation from C# code.
    
    Doxygen produces output in `./doxybuild/xml` (relative to `conf.py`) and exhale
    produces C# output in `./unity-api/`.
    
    In a departure from the recommended exhale approach, we're using a standalone
    Doxyfile and having exhale point directly at it, rather than using exhale's
    standard Doxygen configuration values with a few selected overrides. There are
    a few values (for both breathe and exhale) that MUST coincide with their values
    in the Doxyfile - for now, there is no deduplication, and we just have to make
    sure that they stay in sync.
    
    For no particular reason, I've selected the placeholder HTML theme to be
    alabaster (the default) and the placeholder Pygments style to be solarized-light
    because I think it looks nice. However, these can be reconfigured in the future.
    
    Lastly, this build depends on forked versions of breathe and exhale with minor
    patches, as well as a sizeably revamped C# domain extension. Unfortunately, the
    latest version of these libraries isn't yet on PyPI or GitHub, so I'll next have
    to sync all of the local changes before RTD will be able to pull those
    dependencies and build the documentation remotely.

--- source/conf.py
+++ source/conf.py
@@ -1,52 +1,111 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# This file only contains a few common options. For a full list, see:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'hedron-docs'
-copyright = '2020, Hedron Vision Incorporated'
-author = 'Hedron Vision Incorporated'
+author = "Hedron Vision Incorporated"
+copyright = "2020, Hedron Vision Incorporated"
+version = '0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Names of extension modules, as strings.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-extensions
 extensions = [
+    'sphinxcontrib.csharpdomain',
+    'breathe',
+    'exhale',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# Glob-style patterns to exclude when looking for source files.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-exclude_patterns
+exclude_patterns = [
+    'doxybuild/',
+    'Doxyfile',
+]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# Our default domain is C#, not Python (the usual default).
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-primary_domain
+primary_domain = 'csharp'
+
+# Minimum acceptable version of Sphinx.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-needs_sphinx
+needs_sphinx = '3.0'
+
+# Force Sphinx to execute in nitpicky mode.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-nitpicky
+nitpicky = True
+
+# Tell Sphinx to use the Pygments C# lexer.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-highlight_language
+# See: https://pygments.org/docs/lexers/#pygments.lexers.dotnet.CSharpLexer
+highlight_language = 'csharp'
+
+# Tell Pygments to highlight source code with the solarized theme.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-pygments_style
+# See: https://pygments.org/docs/styles/
+pygments_style = 'solarized-light'
 
 
 # -- Options for HTML output -------------------------------------------------
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# The theme for HTML output.
+# NOTE: This can be changed to hedron_sphinx_theme once the theme is up on GitHub.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_theme
 html_theme = 'alabaster'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
+# -- Options for breathe/exhale ----------------------------------------------
+# See: https://breathe.readthedocs.io/en/latest/quickstart.html
+# See: https://exhale.readthedocs.io/en/latest/usage.html#quickstart-guide
+
+# Tell breathe where to find doxygen's XML output for each project.
+# NOTE: This MUST match the path defined by OUTPUT_DIRECTORY and XML_OUTPUT from
+# the Doxyfile used when the doxygen output was built.
+# See: https://breathe.readthedocs.io/en/latest/directives.html#confval-breathe_projects
+breathe_projects = {
+    'hedron-docs': './doxybuild/xml'
+}
+
+# Tell breathe which project should be the default for all directives.
+# See: https://breathe.readthedocs.io/en/latest/directives.html#confval-breathe_default_project
+breathe_default_project = 'hedron-docs'
+
+# Tell breathe to treat *.cs files using the csharp domain.
+# Without this option, breathe will silently fail to produce any output, besides
+# the object signature. The default mapping associates {'py': 'py', 'cs': 'cs'}.
+# That's a pretty good argument for updating the C# domain to support using 'cs'
+# as a shorthand for 'csharp', if possible.
+# See: https://breathe.readthedocs.io/en/latest/directives.html#confval-breathe_domain_by_extension
+breathe_domain_by_extension = {
+    'cs': 'csharp'
+}
+
+# Set up the exhale extension.
+# See: https://exhale.readthedocs.io/en/latest/usage.html#setup-the-extensions-in-conf-py
+# Note: There are _many_ more available arguments.
+exhale_args = {
+    # Required Arguments
+    'containmentFolder':     './unity-api',
+    'rootFileName':          'root.rst',
+    'rootFileTitle':         "Unity API Reference",
+    'doxygenStripFromPath':  '..',  # This MUST match Doxyfile's STRIP_FROM_PATH.
+    # Optional Arguments
+    # Ask exhale to run doxygen when `make html` is executed.
+    # See: https://exhale.readthedocs.io/en/latest/reference/configs.html#exhale.configs.exhaleExecutesDoxygen
+    'exhaleExecutesDoxygen': True,
+    # We're currently handling all doxygen configuration with a Doxyfile instead
+    # of using 'exhaleDoxygenStdin' (which adds custom tags, such as INPUT) on
+    # top of exhale.configs.DEFAULT_DOXYGEN_STDIN_BASE.
+    # See: https://exhale.readthedocs.io/en/latest/usage.html#using-exhale-to-execute-doxygen
+    # See: https://exhale.readthedocs.io/en/latest/reference/configs.html#exhale.configs.exhaleUseDoxyfile
+    'exhaleUseDoxyfile': True
+}
